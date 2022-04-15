e415b3d6a6283db51d44c6db9f9325e0f78bcb08
kronys121 <woor2009@bk.ru>
2022-04-16 02:09:34 +0500

    каво

--- First.py
+++ First.py
@@ -1,15 +1,13 @@
+zoo_mass = {
+    'lion' : 300,
+    'cat'  : 4,
+    'dog'  : 15,
+    
+}
+total = 0
 
-x = int(input('Введите число : '))
-
-
-if 3 < x < 30:
-
-    print('Ухади')
-elif x == 3:
-    print('Меньше 3')
-elif x > 30:
-    print('Больше 30')
-
-else:
-    print("Ку")
-  
+for animal in zoo_mass:
+    mass = zoo_mass[animal]
+    print(animal, mass)
+    total += mass
+print('Весс всех животных', total)
